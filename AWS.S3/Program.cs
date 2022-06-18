b6a641ba03745411161e47351157744b7fa29404
Fernando Caires <fernando.caires10@outlook.com>
2022-06-18 01:48:44 -0300

    Criação do S3

---
+++ AWS.S3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+
+namespace AWS.S3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host  = Host.CreateDefaultBuilder()
+                        .ConfigureServices((context,services) => 
+                        {
+                            
+                        }).Build();
+        }
+    }
+}
