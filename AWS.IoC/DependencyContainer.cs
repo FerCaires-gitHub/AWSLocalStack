e2f2b44c4ecf2f48c1769b2c7d89a04c051d7d45
Fernando Caires <fernando.caires10@outlook.com>
2022-06-28 20:44:40 -0300

    Comit

--- AWS.IoC/DependencyContainer.cs
+++ AWS.IoC/DependencyContainer.cs
@@ -1,12 +1,15 @@
 ﻿using System;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace AWS.IoC
 {
     public class DependencyContainer
     {
-        public static void RegisterServices(IServiceCollection services)
+        public static void RegisterServices(IServiceCollection services, IConfiguration configuration)
         {
+            var options = configuration.GetAWSOptions("Aws:Localstack");
+            services.AddDefaultAWSOptions(options);
             
         }
     }
